a5d68084cacc70a73f3c613094f9737b906254a8
Jared Harvick <jharvick38@gmail.com>
2023-03-26 17:31:51 -0500

    Added styling for watchlist rows/cols

--- src/ItemsIndex.jsx
+++ src/ItemsIndex.jsx
@@ -6,19 +6,24 @@ export function ItemsIndex(props) {
   return (
     <div>
       <h1>Watchlist</h1>
-      Search: <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
-      {props.items
-        .filter((item) => item.name.toLowerCase().includes(searchFilter.toLowerCase()))
-        .map((item) => (
-          <div key={item.id} className="items">
-            <h2>{item.name}</h2>
-            <img src={item.image_url} alt="" />
-            <p>Description: {item.description}</p>
-            <p>Category: {item.category}</p>
-            <button onClick={() => props.onShowItem(item)}>More info</button>
-            <button onClick={() => props.onCreateFavorite(favorite)}>Add favorite</button>
-          </div>
-        ))}
+      <div className="row">
+        {props.items
+          .filter((item) => item.name.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map((item) => (
+            <div key={item.id} className="col-sm-4 mb-4">
+              <div className="card">
+                <h2 className="card-title">{item.name}</h2>
+                <div className="card-body">
+                  <img src={item.image_url} className="card-img-top-200" alt="" />
+                  <p className="card-text">Description: {item.description}</p>
+                  <p className="card-text">Category: {item.category}</p>
+                  <button onClick={() => props.onShowItem(item)}>More info</button>
+                  <button onClick={() => props.onCreateFavorite(favorite)}>Add favorite</button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
